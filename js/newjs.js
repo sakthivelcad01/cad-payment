7ef4e1fa496a513b4ebabfdbfa49c921a083f414
sakthivelcad01 <jerrycad01@gmail.com>
2024-01-02 15:40:10 +0530

    new javascript file added

---
+++ js/newjs.js
@@ -0,0 +1,11 @@
+
+function opereator(number1,number2){
+    console.log(number1+number2);
+    console.log(number1-number2);
+    console.log(number1*number2);
+    console.log(number1/number2);
+    console.log(number1%number2);
+}
+var number1 = 100;
+var number2 = 200;
+opereator(number1,number2)
