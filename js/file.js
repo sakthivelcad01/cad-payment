909fa0db12a90f6823d31990522eb58efe36be3e
sakthivelcad01 <jerrycad01@gmail.com>
2024-02-06 16:00:39 +0530

    all files modified

--- js/file.js
+++ js/file.js
@@ -1,17 +1,22 @@
-/*document.getElementById().innerHTML=document.getElementById("username").Value;
-document.getElementById().innerHTML=document.getElementById("fname").Value;
-document.getElementById().innerHTML=document.getElementById("lname").Value;
-document.getElementById().innerHTML=document.getElementById("email").Value;
-document.getElementById().innerHTML=document.getElementById("phone").Value;
-document.getElementById().innerHTML=document.getElementById("DOB").Value;*/
 
-function opereator(number1,number2){
-    console.log(number1+number2);
-    console.log(number1-number2);
-    console.log(number1*number2);
-    console.log(number1/number2);
-    console.log(number1%number2);
-}
-var number1 = 100;
-var number2 = 200;
-opereator(number1,number2)
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
